de3c3a56eaf7396f87c462204231a19209131b9d
zhaoweih <zhaoweih@chanjet.com>
2019-01-21 18:13:22 +0800

    finished hikari datasource & mybatis framework

--- src/main/java/com/github/binarywang/demo/wx/open/mapper/WxPublicAuthMapper.java
+++ src/main/java/com/github/binarywang/demo/wx/open/mapper/WxPublicAuthMapper.java
@@ -1,8 +1,6 @@
 package com.github.binarywang.demo.wx.open.mapper;
 
 import com.github.binarywang.demo.wx.open.model.WxPublicAuth;
-import org.apache.ibatis.annotations.Result;
-import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
