2ff31964e6937d75596f4fdfb078a99731259e0a
zhaoweih <zhaoweih@chanjet.com>
2019-01-22 21:38:59 +0800

    lombok

--- src/main/java/com/github/binarywang/demo/wx/open/model/WeixinPublicAuth.java
+++ src/main/java/com/github/binarywang/demo/wx/open/model/WeixinPublicAuth.java
@@ -1,11 +1,15 @@
 package com.github.binarywang.demo.wx.open.model;
 
 import lombok.Builder;
+import lombok.Data;
+import lombok.ToString;
 
 import java.io.Serializable;
 import java.util.Date;
 
 @Builder
+@Data
+@ToString
 public class WeixinPublicAuth implements Serializable {
 
     private static final long serialVersionUID = 1L;
@@ -19,58 +23,4 @@ public class WeixinPublicAuth implements Serializable {
     private Date updateTime;
 
     private Date expiredTime;
-
-    public WeixinPublicAuth() {
-    }
-
-    public String getAppId() {
-        return appId;
-    }
-
-    public void setAppId(String appId) {
-        this.appId = appId;
-    }
-
-    public int getStatus() {
-        return status;
-    }
-
-    public void setStatus(int status) {
-        this.status = status;
-    }
-
-    public Date getCreateTime() {
-        return createTime;
-    }
-
-    public void setCreateTime(Date createTime) {
-        this.createTime = createTime;
-    }
-
-    public Date getUpdateTime() {
-        return updateTime;
-    }
-
-    public void setUpdateTime(Date updateTime) {
-        this.updateTime = updateTime;
-    }
-
-    public Date getExpiredTime() {
-        return expiredTime;
-    }
-
-    public void setExpiredTime(Date expiredTime) {
-        this.expiredTime = expiredTime;
-    }
-
-    @Override
-    public String toString() {
-        return "WeixinPublicAuth{" +
-                "appId='" + appId + '\'' +
-                ", status=" + status +
-                ", createTime=" + createTime +
-                ", updateTime=" + updateTime +
-                ", expiredTime=" + expiredTime +
-                '}';
-    }
 }
