ab2d1da9b45063d59199e43eb61f6ac649f4f7e6
zhaoweih <zhaoweih@chanjet.com>
2019-01-24 12:31:07 +0800

    调整一个空格

--- src/main/java/com/github/binarywang/demo/wx/open/mapper/LinqubatorArticleMapper.java
+++ src/main/java/com/github/binarywang/demo/wx/open/mapper/LinqubatorArticleMapper.java
@@ -12,9 +12,8 @@ import org.springframework.stereotype.Repository;
 public interface LinqubatorArticleMapper {
 
     @Insert("INSERT INTO  " +
-            " (cover, title, content, author, summary, create_time, update_time)" +
-            " VALUES(#{cover}, #{title}, #{content}, #{author}, #{summary}, " +
-            "    #{createTime}, #{updateTime}) ")
-    @Options(useGeneratedKeys = true, keyProperty = "id", keyColumn = "id")
+            " (cover, title, content, author, summary, create_time, update_time) " +
+            " VALUES (#{cover}, #{title}, #{content}, #{author}, #{summary}, #{createTime}, #{updateTime}) ")
+    @Options(useGeneratedKeys = true, keyColumn = "id")
     int insert(LinqubatorArticle linqubatorArticle);
 }
